a6eacd3c42f12cc2c5e07a47771858e933ffbd56
Suthar98 <sutharvictor10@gmail.com>
2024-06-22 23:51:05 +0530

    #3, Create App component separately and used it in index.js file

---
+++ my-app/src/app.js
@@ -0,0 +1,12 @@
+import { Component } from "react"
+
+class App extends Component{
+    render() {
+      return (
+        <div>
+          <h1>Hello Sutharshanan</h1>
+        </div>
+      )
+    }
+  }
+export default App
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './app';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello Sutharshanan</h1>
-      </div>
-    )
-  }
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
