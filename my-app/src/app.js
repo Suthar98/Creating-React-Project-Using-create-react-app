45cac9bc0065ea31cd7b2fb72a81400d245c217e
Suthar98 <sutharvictor10@gmail.com>
2024-06-23 23:14:59 +0530

    #4, Using class componet

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,10 +1,14 @@
 import { Component } from "react"
+import Table from "./Table"
 
 class App extends Component{
     render() {
       return (
         <div>
           <h1>Hello Sutharshanan</h1>
+          <Table/>
+          <h1>Hello Sutharshanan</h1>
+          <Table/>
         </div>
       )
     }
